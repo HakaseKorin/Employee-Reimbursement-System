41b38635b96c1e438602e4494da657a42b286ca0
hakasekorin <aegudes@gmail.com>
2022-02-03 10:18:13 -0600

    fixing project packages

---
+++ ProjectOne/app/src/main/java/com/revature/util/ConnectionUtil.java
@@ -0,0 +1,14 @@
+package com.revature.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionUtil {
+    /*public static Connection getConnection() throws SQLException{
+        String url = System.getenv();
+        String user = System.getenv();
+        String pass = System.getenv();
+        return DriverManager.getConnection(url,user,pass);
+    }*/
+}
