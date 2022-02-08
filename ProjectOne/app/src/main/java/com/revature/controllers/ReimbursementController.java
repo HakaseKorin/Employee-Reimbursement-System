6a4b1075827433780fb8d77b0ffa1843f25862d4
Arby Gudes <aegudes@gmail.com>
2022-02-08 16:05:51 -0600

    adding ReimbursementController

---
+++ ProjectOne/app/src/main/java/com/revature/controllers/ReimbursementController.java
@@ -0,0 +1,52 @@
+package com.revature.controllers;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.revature.models.Reimbursement;
+import com.revature.services.ReimbursementService;
+import io.javalin.http.Context;
+import io.javalin.http.Handler;
+
+public class ReimbursementController {
+
+    private ReimbursementService rs;
+    private ObjectMapper mapper = new ObjectMapper();
+
+    public Handler create = (context) -> {
+        Reimbursement r = mapper.readValue(context.body(), Reimbursement.class);
+
+        rs.createReimbursement(r.getId(), r.getAmount(), r.getSubmitted(), r.getResolved(), r.getDescription(),
+                r.getAuthor(), r.getResolver(), r.getStatusId(), r.getTypeId());
+
+        context.result(mapper.writeValueAsString(r));
+    };
+
+    public Handler update = (context) -> {
+        Reimbursement r = mapper.readValue(context.body(), Reimbursement.class);
+
+        rs.createReimbursement(r.getId(), r.getAmount(), r.getSubmitted(), r.getResolved(), r.getDescription(),
+                r.getAuthor(), r.getResolver(), r.getStatusId(), r.getTypeId());
+
+        context.result(mapper.writeValueAsString(r));
+    };
+
+    public Handler getAll = (context) -> {
+        context.result(mapper.writeValueAsString(rs.getAll()));
+    };
+
+    public Handler getAllPending = (context) -> {
+        context.result(mapper.writeValueAsString(rs.getAllPending()));
+    };
+
+    public Handler getAllResolved = (context) ->{
+        context.result(mapper.writeValueAsString(rs.getAllResolved()));
+    };
+
+    public Handler getById = (context) -> {
+        Integer id = Integer.parseInt(context.pathParam("id"));
+
+        context.result(mapper.writeValueAsString(rs.getById(id)));
+    };
+
+
+
+}
